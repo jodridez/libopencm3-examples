43f51012a8a370fa0004c700241fd29a3bf8c8ae
Jonathan <jonathan.rodriguezhernandez@ucr.ac.cr>
2025-09-30 15:39:29 -0600

    PB13 50% ciclo

--- examples/stm32/f4/stm32f429i-discovery/timerblink/timerblink.c
+++ examples/stm32/f4/stm32f429i-discovery/timerblink/timerblink.c
@@ -140,16 +140,16 @@ static void tim_setup(void)
 	 * might not be the raw APB1/APB2 clocks.  In various conditions they
 	 * are doubled.  See the Reference Manual for full details!
 	 */
-	timer_set_prescaler(TIM1, 4095); // 2563Hz clk
+	timer_set_prescaler(TIM1, 4095); // 4095 para 62.5 ms en PC5, por defecto: xFFFF, 2563Hz clk
     //timer_set_repetition_counter(TIM1, 15);
     timer_disable_preload(TIM1);
     timer_continuous_mode(TIM1);
 
     /* Count period */
-	timer_set_period(TIM1, 2563); 
+	timer_set_period(TIM1, 2563); // 2563 por defecto
 
 	/* Set the initual output compare value for OC1. */
-	timer_set_oc_value(TIM1, TIM_OC1, 320); // no usar los negativos
+	timer_set_oc_value(TIM1, TIM_OC1, 1282); // no usar los negativos // 320 por defecto 
 
     /* Disable outputs. */
     //timer_enable_oc_output(TIM1, TIM_OC1);
